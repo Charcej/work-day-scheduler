1afef988393c8bcef0b59046b057f598cec2dc64
charles johnston <charlesjohnston2002@yahoo.com>
2022-01-23 21:59:39 -0500

    basic grid layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-// don't know if I need this file but it's here just in case
+// This is for discovering today's day, month and year and consoling it with Moment.js
+var todaysDate = moment().format('dddd, MMMM Do');
+console.log(todaysDate);
+// This is for displaying the variable for 'todayDate' on the page inside p element with id of "currentDay" with jQuery
+$("#currentDay").html(todaysDate);
+
