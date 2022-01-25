5584df1b34188751d3412bd474c3739e8185b266
charles johnston <charlesjohnston2002@yahoo.com>
2022-01-25 16:59:47 -0500

    started js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-// This is for discovering today's day, month and year and consoling it with Moment.js
+// This is for discovering today's day, month and year with Moment.js
 var todaysDate = moment().format('dddd, MMMM Do');
+// to make sure it's working
 console.log(todaysDate);
 // This is for displaying the variable for 'todayDate' on the page inside p element with id of "currentDay" with jQuery
 $("#currentDay").html(todaysDate);
-
