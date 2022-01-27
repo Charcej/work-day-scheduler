d5ed807259d1d0ef499f233530ed3b00f78eca85
charles johnston <charlesjohnston2002@yahoo.com>
2022-01-26 21:41:27 -0500

    tidied up js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,24 @@
 var todaysDate = moment().format('dddd, MMMM Do');
 // to make sure it's working
 console.log(todaysDate);
-// This is for displaying the variable for 'todayDate' on the page inside p element with id of "currentDay" with jQuery
+// This is for displaying 'todayDate' on the page, inside p element, with id of "currentDay" (with jQuery)
 $("#currentDay").html(todaysDate);
 
 $(document).ready(function () {
     // this is to set up a click listener for save button targeted by saveBtn class
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
+        // Get nearby values of the description and time of day (with JQuery)
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // This is how to save the text entered into local storage
+        // This is how to save the text and time associated with it that was entered and saved into local storage
         localStorage.setItem(time, text);
     })
-   
+
     function timeTracker() {
         // this is how to get the current number in terms of hours
         var timeNow = moment().hour();
+        console.log(timeNow)
 
         // this is so you can loop over the time blocks
         $(".time-block").each(function () {
@@ -30,7 +31,7 @@ $(document).ready(function () {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
-            // this is for is the time is now and so would add background indicator to show it's in the present and adds that bg color (red)
+                // this is for is the time is now and so would add background indicator to show it's in the present and adds that bg color (red)
             }
             else if (timeBlock === timeNow) {
                 $(this).removeClass("past");
@@ -47,7 +48,7 @@ $(document).ready(function () {
         })
     }
 
-    // this is for getting an item from local storage if there is one to get for every single timeblock
+    // this is for getting an item from local storage if there is one to get (for every single timeblock)
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -59,10 +60,9 @@ $(document).ready(function () {
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-    // for calling the timeTracker function
+    // for calling the timeTracker function which gets the time in hours
     timeTracker();
 })
 
-   
-  
-    
+
+
